d6db89338077d115b18b9a411a919d29a24a634d
Misha Djak <23djak@gmail.com>
2020-05-13 18:57:52 +0300

    Fix

--- resources/views/calendar/catalog/item.blade.php
+++ resources/views/calendar/catalog/item.blade.php
@@ -1,9 +1,9 @@
 <div class="row gnc-calendar-list-day">
-    <div class="gnc-calendar-list-day--header small-2 small-offset-1 medium-2 medium-offset-2">
+    <div class="gnc-calendar-list-day--header small-3 medium-2 medium-offset-2">
         <div class="gnc-calendar-list-day--date">{{ $date }}</div>
         <div class="gnc-calendar-list-day--name">{{ $name }}</div>
     </div>
-    <div class="gnc-calendar-list-day--details small-6">
+    <div class="gnc-calendar-list-day--details small-9 medium-6">
         @foreach($sheet as $item)
             <span class="gnc-calendar-list-day--point">
                 <span class="gnc-calendar-list-day--time"> {{ $item['time'] }} </span>
