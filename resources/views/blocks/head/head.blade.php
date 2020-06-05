2e476ea53e54bef83cc1ef7ab1baa3c4e8dd3a56
mdyachenko <23djak@gmail.com>
2020-06-05 09:38:07 +0300

    Add livewire

--- resources/views/blocks/head/head.blade.php
+++ resources/views/blocks/head/head.blade.php
@@ -15,4 +15,5 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    @livewireStyles
 </head>
